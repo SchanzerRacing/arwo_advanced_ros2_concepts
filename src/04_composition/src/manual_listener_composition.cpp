57a55327f514e26c0c0c5ea25c5145337ed98fd8
Leonce Mollerus <leonce.mollerus@schanzer-racing.de>
2024-03-11 07:30:16 +0000

    add manual composition to cmakelists and fix missing node options

--- src/04_composition/src/manual_listener_composition.cpp
+++ src/04_composition/src/manual_listener_composition.cpp
@@ -1,13 +1,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include "04_composition/listener_component.hpp"
-#include "04_composition/heatbeat_sub_component.hpp"
+#include "04_composition/heartbeat_sub_component.hpp"
 
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::executors::MultiThreadedExecutor executor;
-    auto listener = std::make_shared<arwo::ListenerNode>();
-    auto heartbeat_sub = std::make_shared<arwo::HeartbeatSubNode>();
+    rclcpp::NodeOptions node_options;
+    node_options.use_intra_process_comms(true);
+    auto listener = std::make_shared<arwo::ListenerNode>(node_options);
+    auto heartbeat_sub = std::make_shared<arwo::HeartbeatSubNode>(node_options);
     executor.add_node(listener);
     executor.add_node(heartbeat_sub);
     executor.spin();
--- src/04_composition/src/manual_talker_composition.cpp
+++ src/04_composition/src/manual_talker_composition.cpp
@@ -1,14 +1,17 @@
 #include <rclcpp/rclcpp.hpp>
-#include "04_composition/talker_component.hpp"
+#include "04_composition/lifecycle_talker_component.hpp"
 #include "04_composition/heartbeat_pub_component.hpp"
 
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::executors::MultiThreadedExecutor executor;
-    auto talker = std::make_shared<arwo::TalkerNode>();
-    auto heartbeat_pub = std::make_shared<arwo::HeartbeatPubNode>();
-    executor.add_node(talker);
+
+    rclcpp::NodeOptions node_options;
+    node_options.use_intra_process_comms(true);
+    auto lifecycle_talker = std::make_shared<arwo::LifecycleTalkerNode>(node_options);
+    auto heartbeat_pub = std::make_shared<arwo::HeartbeatPubNode>(node_options);
+    executor.add_node(lifecycle_talker->get_node_base_interface());
     executor.add_node(heartbeat_pub);
     executor.spin();
     rclcpp::shutdown();
